470982543f2bacd9b03c86e403cf3c5e61690883
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-28 20:23:54 +0200

    8 fahrenheit to celsius

---
+++ session01/exercises-01/08_fahrenheit_to _celsius.py
@@ -0,0 +1,20 @@
+# 8. from fahrenheit to celsius
+
+#skriver fem verdier
+fahrenheit_degree1 = float(35)
+fahrenheit_degree2 = float(68)
+fahrenheit_degree3 = float(108)
+fahrenheit_degree4 = float(0)
+fahrenheit_degree5 = float(-14)
+
+celsius_degree1 = float((fahrenheit_degree1 - 32) * 5 / 9)
+celsius_degree2 = float((fahrenheit_degree2 - 32) * 5 / 9)
+celsius_degree3 = float((fahrenheit_degree3 - 32) * 5 / 5)
+celsius_degree4 = float((fahrenheit_degree4 - 32) * 5 / 9)
+celsius_degree5 = float((fahrenheit_degree5 - 32) * 5 / 9)
+
+print(celsius_degree1)
+print(celsius_degree2)
+print(celsius_degree3)
+print(celsius_degree4)
+print(celsius_degree5)
