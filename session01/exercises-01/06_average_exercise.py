4ae939e7f09ecb3c1a6505b91f22c09e33c99eb8
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-28 19:48:25 +0200

    6. finne gjennomsnittet

---
+++ session01/exercises-01/06_average_exercise.py
@@ -0,0 +1,17 @@
+# 6.find average
+
+orange = int(6)
+apple = float(12)
+pear = int(15)
+grape = float(9)
+kiwi = int (25)
+
+type_of_fruits = float(5)
+
+total_fruits = int(orange + apple + pear + grape + kiwi)
+
+print (total_fruits)
+
+print(total_fruits/type_of_fruits)
+
+# finding average of fruits
