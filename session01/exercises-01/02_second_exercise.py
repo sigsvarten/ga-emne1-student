c250ac268b9b9d894947c58e7ba98b4e625052cd
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-28 15:09:50 +0200

    5. Summer to tall og konvertere

---
+++ session01/exercises-01/02_second_exercise.py
@@ -0,0 +1,25 @@
+# Use Int and float
+
+#5. convert input to numbers and adders
+
+#first pair
+number = input ('What is the first number? ')
+second_number = input('What is the second number? ')
+
+print (int(f'{number}') + int(f'{second_number}'))
+
+#second_pair
+third_number = input ('What is the this number? ')
+fourth_number = input('What is the this number? ')
+
+print (int(f'{third_number}') + int(f'{fourth_number}'))
+
+#third pair
+fifth_number = input ('What is the number here? ')
+sixth_number = input('This number is? ')
+
+print (int(f'{fifth_number}') + int(f'{sixth_number}'))
+
+#regner ut alle tall
+print (int(f'{fifth_number}') + int(f'{sixth_number}') + int(f'{number}') + int(f'{second_number}') +
+       int(f'{third_number}') + int(f'{fourth_number}'))
