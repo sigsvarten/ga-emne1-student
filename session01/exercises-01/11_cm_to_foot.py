9b5eb1043195f55197d6c89957fa987db2b3a412
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-29 12:33:31 +0200

    nummr 11 : cm og fot

---
+++ session01/exercises-01/11_cm_to_foot.py
@@ -0,0 +1,22 @@
+#Konvertere fra cm til fot , konvertere med ein oppgitt faktor
+
+#leveranse 1
+height_cm = float(input ('What is your height? '))
+
+height_foot = (height_cm / 30.48)
+
+print(f'Your height in foot is: {height_foot}')
+
+#leveranse 2
+height_cm2 = float(input ('What is your height? '))
+
+height_foot2 = (height_cm2 / 30.48)
+
+print(f'Your height in foot is: {height_foot2}')
+
+#leveranse 3
+height_cm3 = float(input ('What is your height? '))
+
+height_foot3 = (height_cm3 / 30.48)
+
+print(f'Your height in foot is: {height_foot3}')
