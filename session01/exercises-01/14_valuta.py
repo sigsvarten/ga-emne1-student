b66a0555b2ca46ca1ee996bba2042acf6c9df034
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-29 23:54:15 +0200

    nummer 14 : valuta

---
+++ session01/exercises-01/14_valuta.py
@@ -0,0 +1,25 @@
+#valutaomregning
+
+#test1
+amount = float(input('What is the amount? '))
+course_test = float(input('Tap in the course '))
+
+currency = float(amount * course_test)
+
+print( 'Total amount is: ' + f'{currency}')
+
+#test2
+amount2 = float(input('What is this amount? '))
+course_test2 = float(input('Tap in the course '))
+
+currency2 = float(amount2 * course_test2)
+
+print( 'Total amount is: ' + f'{currency2}')
+
+#test3
+amount3 = float(input('What is this amount? '))
+course_test3 = float(input('Tap in the course '))
+
+currency3 = float(amount3 * course_test3)
+
+print( 'Total amount is: ' + f'{currency3}')
