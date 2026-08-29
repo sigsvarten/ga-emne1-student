1a194b9a417c38e06038e5fac46183a9ea2278bd
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-29 12:51:47 +0200

    nummr 12 : vekelønn

---
+++ session01/exercises-01/12_weekly_wage.py
@@ -0,0 +1,25 @@
+# beregne vekelønn, moddellere praktisk beregning
+
+#leveranse 1
+price_hour = float(input('The price per hour is: '))
+hours = float(input('Total hours is: '))
+
+weekly_wages = price_hour * hours
+
+print (f'Total wages is: {weekly_wages}')
+
+#leveranse 2
+price_hour2 = float(input('The price per hour is: '))
+hours2 = float(input('Total hours is: '))
+
+weekly_wages2 = price_hour2 * hours2
+
+print (f'Total wages is: {weekly_wages2}')
+
+#leveranse 3
+price_hour3 = float(input('The price per hour is: '))
+hours3 = float(input('Total hours is: '))
+
+weekly_wages3 = price_hour3 * hours3
+
+print (f'Total wages is: {weekly_wages3}')
