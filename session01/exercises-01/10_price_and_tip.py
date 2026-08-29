a7df395176353d848447bdb02b031c2478b35afc
sigsvarten <sig.hat090@gokstadakademiet.no>
2026-08-29 12:17:06 +0200

    nummr 10 : prosent og tips

---
+++ session01/exercises-01/10_price_and_tip.py
@@ -0,0 +1,28 @@
+#bruke prosentregning
+
+#leveranse 1
+input_price = float (input('The price is: '))
+
+tip = (input_price * 0.15)
+total_price = (tip + input_price)
+
+print(f' The tip is {tip} ')
+print(f' Total price is {total_price} ')
+
+#leveranse 2
+input_price2 = float (input('This price is: '))
+
+tip2 = (input_price2 * 0.15)
+total_price2 = (tip2 + input_price2)
+
+print(f' This tip is {tip2} ')
+print(f' The second total price is {total_price2} ')
+
+#Leveranse 3
+input_price3 = float (input('The third price is: '))
+
+tip3  = (input_price3 * 0.15)
+total_price3 = (tip3 + input_price3)
+
+print(f' The third tip is {tip3} ')
+print(f' This price is {total_price3} ')
